7f83152f87c12d60d3a9fdff603f44e98dbeabfd
Rain <191729342+Goal-max@users.noreply.github.com>
2025-12-15 06:26:07 +1100

    2nd exercise stuck syntax error

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -2,10 +2,12 @@ const removeFromArray = function(array, ...args) {
  //first know length of ...args []
 //create loop which goes through index of ...args [] and assign to variable
 //the loop will then splice the variable from array
-  for (let i = 0; i  < ...args.length; i++) {
-    let index = array.indexOf(arg); 
-    array.splice(index, 1);
-    return array;
+  let arg = ...args;
+  for (let i = 0; i  < ...arg.length; i++) {
+    get remove = arg[i];
+    array.splice(remove, 1);
+  }
+  return array;
 };
 
 // Do not edit below this line
