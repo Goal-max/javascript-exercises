ac31a7f6b8f6b522b4200e218e63bf33a29302c9
Rain <191729342+Goal-max@users.noreply.github.com>
2025-12-16 20:17:58 +1100

    exercise 8  pass 2nd test

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -2,12 +2,10 @@ const removeFromArray = function(array, ...args) {
  //first know length of ...args []
 //create loop which goes through index of ...args [] and assign to variable
 //the loop will then splice the variable from array
-//  for (let i = 0; i  < args.length; i++) {
-//let remove = args[0];
-//below not working as we are removing based on index instead of removing particular number
-  let index = array.findIndex (element => element === args[0]);
+  for (let i = 0; i  < args.length; i++) {
+    let index = array.findIndex (element => element === args[i]);
     array.splice(index, 1);
-//}
+  }
   return array;
 };
 
