d1ff058f7fdd26186c9438b1cc8c10814758271c
Rain <191729342+Goal-max@users.noreply.github.com>
2025-12-16 20:31:20 +1100

    Update to use proper array method instead of object method

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,9 +1,7 @@
 const removeFromArray = function(array, ...args) {
- //first know length of ...args []
-//create loop which goes through index of ...args [] and assign to variable
-//the loop will then splice the variable from array
   for (let i = 0; i  < args.length; i++) {
-    let index = array.findIndex (element => element === args[i]);
+//while loop to keep searching for index if still in array
+    let index = array.indexOf(args[i]);
     array.splice(index, 1);
   }
   return array;
