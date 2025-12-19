c60345cff34eb7c898b61f2d37b8272ed49fc730
Rain <191729342+Goal-max@users.noreply.github.com>
2025-12-19 21:17:27 +1100

    passed 1st test

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+  let array = [a,b];
+  array.sort();
+  let diff = array[1] - array[0];
+  
+  for (let i = 1; i < diff; i++) {
+    let number = array[0] + i;
+    array.push(number); 
+  }
+  
+  let sum = array.reduce((sum, current) => sum + current, 0);
+  return sum;
 };
 
 // Do not edit below this line
